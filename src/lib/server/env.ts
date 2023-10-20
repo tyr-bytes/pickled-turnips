1048ea4018e149e9cb9e79e63093cb25ed75c0ef
Mike <michaelpaulmccabe@gmail.com>
2023-10-20 03:57:38 +0000

    test

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,4 +1,4 @@
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 dotenv.config({path: `env.local`, override: true})
 function getEnvironmentVariable(environmentVariable: string): string {
